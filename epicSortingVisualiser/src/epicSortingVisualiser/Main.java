3cb59b34b6e5e1a86af49b29b64f93c60d521c62
jonathanplsmith <92918075+jonathanplsmith@users.noreply.github.com>
2022-01-07 14:38:53 +0100

    Added timing

--- epicSortingVisualiser/src/epicSortingVisualiser/Main.java
+++ epicSortingVisualiser/src/epicSortingVisualiser/Main.java
@@ -1,49 +1,90 @@
 package epicSortingVisualiser;
-import java.util.Random;
+import java.util.Arrays;
+
 import sortingAlgos.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//GUI init
-		GUI.draw();
+		//GUI.draw();
 		
-		//Init of array: 255 elements from 0 to 255 which are then shuffled
-		int array[] = new int[256];
-		for (int i=255; i>=0; i--) {
-			array[i] = i;
-		}
-		shuffleArray(array);
-		
-		//call sorting algorithms
-		printArray(array);
-		
-		//array = BubbleSort.bubbleSortDriver(array);
-		//array = SelectionSort.selectionSortDriver(array);
-		//array = InsertionSort.insertionSortDriver(array);
-		//array = HeapSort.heapSortDriver(array);
-		//array = MergeSort.mergeSortDriver(array);
-		array = QuickSort.quickSortDriver(array);
-		
-		printArray(array);
+		//Timing
+		timing();
 	}
 	
-	public static void printArray(int[] a) {
-		//prints an array
-		System.out.print("[");
-		for (int i=0; i<a.length-1; i++) {
-			System.out.print(a[i] + ", ");
+	public static void timing() {
+		int runs = 100;
+		long bubbleTime = 0;
+		long selectionTime = 0;
+		long insertionTime = 0;
+		long heapTime = 0;
+		long mergeTime = 0;
+		long quickTime = 0;
+		long arraysTime = 0;
+		
+		for (int i=0; i<runs; i++) {
+			//Init of array, shuffle
+			int array[] = new int[10000];
+			for (int j=array.length-1; j>=0; j--) {
+				array[j] = j;
+			}
+			Utils.shuffleArray(array);
+			
+			//bubbleSort
+			long bST = System.currentTimeMillis();
+			BubbleSort.bubbleSortDriver(Arrays.copyOf(array, array.length));
+			long bET = System.currentTimeMillis();
+			bubbleTime += delta(bST, bET);
+			
+			//selectionSort
+			long sST = System.currentTimeMillis();
+			SelectionSort.selectionSortDriver(Arrays.copyOf(array, array.length));
+			long sET = System.currentTimeMillis();
+			selectionTime += delta(sST, sET);
+			
+			//insetionSort
+			long iST = System.currentTimeMillis();
+			InsertionSort.insertionSortDriver(Arrays.copyOf(array, array.length));
+			long iET = System.currentTimeMillis();
+			insertionTime += delta(iST, iET);
+			
+			//heapSort
+			long hST = System.currentTimeMillis();
+			HeapSort.heapSortDriver(Arrays.copyOf(array, array.length));
+			long hET = System.currentTimeMillis();
+			heapTime += delta(hST, hET);
+			
+			//mergeSort
+			long mST = System.currentTimeMillis();
+			MergeSort.mergeSortDriver(Arrays.copyOf(array, array.length));
+			long mET = System.currentTimeMillis();
+			mergeTime += delta(mST, mET);
+			
+			//quickSort
+			long qST = System.currentTimeMillis();
+			QuickSort.quickSortDriver(Arrays.copyOf(array, array.length));
+			long qET = System.currentTimeMillis();
+			quickTime += delta(qST, qET);
+			
+			//arraysSort
+			long aST = System.currentTimeMillis();
+			Arrays.sort(Arrays.copyOf(array, array.length));
+			long aET = System.currentTimeMillis();
+			arraysTime += delta(aST, aET);
 		}
-		System.out.println(a[a.length-1] + "]");
+		
+		System.out.println("Bubble Sort Avg.: " + (double) bubbleTime / runs + "ms");
+		System.out.println("Selection Sort Avg.: " + (double) selectionTime / runs + "ms");
+		System.out.println("Insertion Sort Avg.: " + (double) insertionTime / runs + "ms");
+		System.out.println("Heap Sort Avg.: " + (double) heapTime / runs + "ms");
+		System.out.println("Merge Sort Avg.: " + (double) mergeTime / runs + "ms");
+		System.out.println("Quick Sort Avg.: " + (double) quickTime / runs + "ms");
+		System.out.println("Arrays.sort Avg.: " + (double) arraysTime / runs + "ms");
+		
 	}
 	
-	public static void shuffleArray(int[] a) {
-		Random rand = new Random();
-		for (int i=a.length-1; i>=0; i--) {
-		      int swap = rand.nextInt(i + 1);
-		      int temp = a[swap];
-		      a[swap] = a[i];
-		      a[i] = temp;
-		}
+	public static long delta(long l1, long l2) {
+		return l2 - l1;
 	}
 }
---
+++ epicSortingVisualiser/src/epicSortingVisualiser/Utils.java
@@ -0,0 +1,24 @@
+package epicSortingVisualiser;
+
+import java.util.Random;
+
+public class Utils {
+	public static void printArray(int[] a) {
+		//prints an array
+		System.out.print("[");
+		for (int i=0; i<a.length-1; i++) {
+			System.out.print(a[i] + ", ");
+		}
+		System.out.println(a[a.length-1] + "]");
+	}
+	
+	public static void shuffleArray(int[] a) {
+		Random rand = new Random();
+		for (int i=a.length-1; i>=0; i--) {
+		      int swap = rand.nextInt(i + 1);
+		      int temp = a[swap];
+		      a[swap] = a[i];
+		      a[i] = temp;
+		}
+	}
+}
